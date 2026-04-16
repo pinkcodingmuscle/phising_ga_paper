8b01077e6b58bfb628081e054dbecda3ddeb6d4f
Marcela <marcela.s.perro@.com>
2026-04-16 05:37:10 -0600

    evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,14 +1,16 @@
-# from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+def evaluate_model(name, model, X_train, X_test, y_train, y_test):
+    """
+    Fits the model and calculates standard classification metrics.
+    """
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
 
-# def evaluate_model(name, model, X_train, X_test, y_train, y_test):
-#     model.fit(X_train, y_train)
-#     y_pred = model.predict(X_test)
-
-#     return {
-#         "Model": name,
-#         "Accuracy": accuracy_score(y_test, y_pred),
-#         "Precision": precision_score(y_test, y_pred, average="weighted", zero_division=0),
-#         "Recall": recall_score(y_test, y_pred, average="weighted", zero_division=0),
-#         "F1": f1_score(y_test, y_pred, average="weighted", zero_division=0)
-#     }
+    return {
+        "Model": name,
+        "Accuracy": accuracy_score(y_test, y_pred),
+        "Precision": precision_score(y_test, y_pred, average="weighted", zero_division=0),
+        "Recall": recall_score(y_test, y_pred, average="weighted", zero_division=0),
+        "F1": f1_score(y_test, y_pred, average="weighted", zero_division=0)
+    }
